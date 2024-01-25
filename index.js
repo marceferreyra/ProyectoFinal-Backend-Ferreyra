bb01454f41345f46bcd49e7222f819e59a500a5c
Marcelo Ferreyra <marceloferreyra@outlook.com>
2024-01-24 21:56:19 -0300

    agrego websocket

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const express = require('express');
 const productRouter = require('./routes/products.routes.js');
 const cartRouter = require('./routes/carts.routes.js');
-const homeRouter = require (`./routes/home.routes.js`)
-const handlebars = require (`express-handlebars`)
-const http = require (`http`)
-const {Server} =  require (`socket.io`)
+const homeRouter = require(`./routes/home.routes.js`)
+const realTimeProductsRouter = require('./routes/realTimeProducts.routes.js')
+const handlebars = require(`express-handlebars`)
+const http = require(`http`)
+const { Server } = require(`socket.io`)
 const app = express();
 const PORT = 8080;
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-
-
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 app.use(express.json());
 
 app.use(productRouter);
 app.use('/api/carts', cartRouter);
-app.use(`/home`, homeRouter)
+app.use(`/home`, homeRouter);
+app.use('/realtimeproducts', realTimeProductsRouter);
 
-app.engine (`handlebars`, handlebars.engine())
+app.engine(`handlebars`, handlebars.engine())
 app.set(`view engine`, `handlebars`)
 app.set('views', __dirname + '/views')
 
-const io = new Server(server)
-io.on(`connection`, (socket)=> {
-    console.log (`hola`)
-})
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+
+    socket.on('productAdded', (newProduct) => {
+        console.log('Producto agregado:', newProduct);
+        io.emit('updateProducts', newProduct);
+    });
+
+    socket.on('productDeleted', (productId) => {
+        console.log(`Producto con ID ${productId} eliminado`);
+        io.emit('updateProducts', { deletedProductId: productId });
+    });
+});
+
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- routes/realTimeProducts.routes.js
+++ routes/realTimeProducts.routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const ProductManager = require('../src/productManager');
-const homeRouter = express.Router();
+const realTimeProductsRouter = express.Router();
 const productManager = new ProductManager();
 
-homeRouter.get('/', async (req, res) => {
+realTimeProductsRouter.get('/', async (req, res) => {
     try {
         const products = await productManager.getProducts();
-        res.render('layouts/home', { products });
+        res.render('layouts/realTimeProducts', { products });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al obtener productos' });
     }
 });
 
-module.exports = homeRouter;
+module.exports = realTimeProductsRouter
