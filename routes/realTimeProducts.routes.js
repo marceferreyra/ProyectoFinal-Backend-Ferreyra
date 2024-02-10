b82ac4cbce6fbd21b8f665ae5d5e467ae057480d
Marcelo Ferreyra <marceloferreyra@outlook.com>
2024-02-10 18:06:10 -0300

    migracion a mongodb realtimeproducts

--- routes/realTimeProducts.routes.js
+++ routes/realTimeProducts.routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const ProductManager = require('../src/productManager');
+const productManagerMongo = require('../dao/productManagerMongo');
 const realTimeProductsRouter = express.Router();
-const productManager = new ProductManager();
 
 realTimeProductsRouter.get('/', async (req, res) => {
     try {
-        const products = await productManager.getProducts();
-        res.render('layouts/realTimeProducts', { products });
+        const products = await productManagerMongo.getProducts();
+        const plainProducts = products.map(product => product.toObject({ getters: true }));
+        res.render('layouts/realtimeproducts', { products: plainProducts });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error al obtener productos' });
+        res.status(500).json({ error: 'Error al obtener productos desde MongoDB.' });
     }
 });
 
