d00d70684aaa2d0eb5b3b04415d132c66a2398b1
Marcelo Ferreyra <marceloferreyra@outlook.com>
2024-02-21 19:52:11 -0300

    agrego enum a category

--- src/dao/db/models/productModel.js
+++ src/dao/db/models/productModel.js
@@ -8,7 +8,7 @@ const productSchema = new mongoose.Schema({
     code: { type: String, require: true, unique: true },
     stock: { type: Number, required: true },
     status: { type: Boolean, required: true },
-    category: { type: String, required: true }
+    category: { type: String, required: true, enum: ['calzado', 'accesorios', 'camperas'] }
 
 });
 
