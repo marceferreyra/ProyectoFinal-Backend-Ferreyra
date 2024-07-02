93b908d57793576e7e2cb4a3a73981d1e8fb081f
Marcelo Ferreyra <marceloferreyra@outlook.com>
2024-07-02 11:32:17 -0300

    aplico estilos a chat

--- src/dao/db/controllers/chatController.js
+++ src/dao/db/controllers/chatController.js
@@ -1,9 +1,10 @@
 const Message = require('../models/chatModel');
 
 exports.getAllMessages = async (req, res) => {
+    const user = req.session.user
     try {
         const messages = await Message.find().lean(); 
-        res.render('chat', { messages });
+        res.render('chat', { messages, user });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error interno del servidor al obtener los mensajes.' });
